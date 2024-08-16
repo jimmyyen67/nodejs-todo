eddcc7791cf363e069ad713868bfea13c1782b30
Jimmy Yen <ichthyoid95@gmail.com>
2024-08-16 16:12:56 +0800

    add some feature - get all todos routing - express-async-handler

--- src/app.js
+++ src/app.js
@@ -1,9 +1,44 @@
 import express from 'express';
+import connectDB from './database/db.js';
+import asyncHandler from 'express-async-handler';
+import mongoose from 'mongoose';
+import Todo from './models/todo.model.js';
+
 const app = express();
-const port = 3000;
+const port = process.env.port || 3000;
+
+connectDB();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World');
+});
+
+app.get('/todos', asyncHandler(async (req, res) => {
+  const todos = await Todo.find();
+  res.status(200).send({
+    success: true,
+    message: "Todo Lists Retrieved Successfully",
+    data: todos
+  })
+}));
+
+// error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  if (err instanceof mongoose.Error) {
+    return res.status(500).send({
+      success: false,
+      message: 'Database error occurred.',
+      error: err.message
+    });
+  }
+  res.status(500).send({
+    success: false,
+    message: 'Internal Server Error. Please try again later.',
+    error: err.message
+  })
 });
 
 app.listen(port, () => {
---
+++ src/models/todo.model.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+const TodoSchema = new mongoose.Schema({
+    text: {
+        type: String,
+        required: true
+    },
+    priority: {
+        type: String,
+        required: true
+    },
+    deadline: {
+        type: String,
+        required: true
+    }
+});
+
+const Todo = mongoose.models.Todo || mongoose.model('Todo', TodoSchema);
+
+export default Todo;
