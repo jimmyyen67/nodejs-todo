8eee3898c03c989270e52b6790dcb0f786da6672
Jimmy Yen <ichthyoid95@gmail.com>
2024-08-16 17:15:16 +0800

    adding some features - post, put, delete todo-list - some .http client test

--- src/app.js
+++ src/app.js
@@ -16,17 +16,106 @@ app.get('/', (req, res) => {
 });
 
 app.get('/todos', asyncHandler(async (req, res) => {
-  const todos = await Todo.find();
-  res.status(200).send({
-    success: true,
-    message: "Todo Lists Retrieved Successfully",
-    data: todos
-  })
+  try {
+    const todos = await Todo.find();
+    res.status(200).send({
+      success: true,
+      message: "All Todo-lists Retrieved SUCCESSFULLY",
+      data: todos
+    })
+  } catch (err) {
+    err.errorType = "TodoRetrieveError";
+    err.customMessage = "Retrieve All Todo-list FAILED";
+    throw err;
+  }
+
+}));
+
+app.post('/todos', asyncHandler(async (req, res) => {
+  try {
+    const todo = req.body;
+    const result = await Todo.create(todo);
+    res.status(201).send({
+      success: true,
+      message: "The Todo-list Created SUCCESSFULLY",
+      data: result
+    })
+  } catch (err) {
+    err.errorType = "TodoCreateError";
+    err.customMessage = "Insert todo-list FAILED";
+    throw err;
+  }
+}));
+
+app.get('/todos/:id', asyncHandler(async (req, res) => {
+  const id = req.params.id;
+  try {
+    const todo = await Todo.findById(id);
+    res.status(200).send({
+      success: true,
+      message: "The todo-list retrieved SUCCESSFULLY",
+      data: todo
+    })
+  } catch (err) {
+    err.errorType = "TodoRetrieveError";
+    err.customMessage = "Retrieve todo-list FAILED";
+    throw err;
+  }
+}));
+
+app.put('/todos/:id', asyncHandler(async (req, res) => {
+  try {
+    const id = req.params.id;
+    const todo = req.body;
+    const result = await Todo.findByIdAndUpdate(id, todo);
+    if (!result) {
+      throw new Error('Todo not found');
+    }
+    res.status(200).send({
+      success: true,
+      message: "The todo-list updated SUCCESSFULLY",
+      data: result
+    });
+  } catch (err) {
+    err.errorType = "TodoUpdateError";
+    err.customMessage = "Update todo-list FAILED";
+    throw err;
+  }
+}));
+
+app.delete('/todos/:id', asyncHandler(async (req, res) => {
+  try {
+    const id = req.params.id;
+    const result = await Todo.findByIdAndDelete(id);
+    
+    if (!result) {
+      throw new Error('Todo not found');
+    }
+    
+    res.status(200).send({
+      success: true,
+      message: "The todo-list deleted SUCCESSFULLY",
+      data: result
+    });
+  } catch (err) {
+    err.errorType = "TodoDeleteError";
+    err.customMessage = "Delete todo-list FAILED";
+    throw err;
+  }
 }));
 
 // error handler
 app.use((err, req, res, next) => {
   console.error(err.stack);
+
+  if (err.errorType) {
+    return res.status(err.status || 500).send({
+      success: false,
+      message: err.customMessage,
+      error: err.message
+    })
+  }
+
   if (err instanceof mongoose.Error) {
     return res.status(500).send({
       success: false,
@@ -34,6 +123,7 @@ app.use((err, req, res, next) => {
       error: err.message
     });
   }
+
   res.status(500).send({
     success: false,
     message: 'Internal Server Error. Please try again later.',
--- src/models/todo.model.js
+++ src/models/todo.model.js
@@ -1,19 +1,24 @@
 import mongoose from 'mongoose';
 
-const TodoSchema = new mongoose.Schema({
-    text: {
-        type: String,
-        required: true
+const TodoSchema = new mongoose.Schema(
+    {
+        text: {
+            type: String,
+            required: true
+        },
+        priority: {
+            type: String,
+            required: true
+        },
+        deadline: {
+            type: String,
+            required: true
+        }
     },
-    priority: {
-        type: String,
-        required: true
-    },
-    deadline: {
-        type: String,
-        required: true
+    {
+        timestamps: true
     }
-});
+);
 
 const Todo = mongoose.models.Todo || mongoose.model('Todo', TodoSchema);
 
